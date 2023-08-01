29d0bdf18a5da5ce5ac00a6c5d5ba430362871d3
YUVRAJ KUMAR ARORA <110113664+Officialyuvi070@users.noreply.github.com>
2023-08-01 11:51:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './counter';
+
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+
+export default App;
